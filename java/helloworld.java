548e10cafad3199f61c49156324a589dfc4628ce
Wesley939 <wesleyassisdelvalle08@gmail.com>
2025-07-25 17:44:01 -0300

    cdm_nome_idade

--- java/helloworld.java
+++ java/helloworld.java
@@ -1,6 +1,21 @@
+import java.util.Scanner;
+
 public class helloworld {
 
     public static void main(String[] args) {
         System.out.println("ola mundo");
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Digite seu idade: ");
+        int idade = input.nextInt();
+
+        System.out.print("qual o seu nome: ");
+        String nome = input.next();
+
+        System.out.printf("sua idade: %s",idade);
+        System.out.println();
+        System.out.printf("seu nome: %s",nome);
+        System.out.println();
+
     }
 }
